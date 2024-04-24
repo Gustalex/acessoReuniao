be6aed2dbc793bf7aa3205cee14f2bcca501b723
Enéas <david00eneas@gmail.com>
2024-04-24 11:26:56 -0300

    correção da arquitetura do projeto

--- src/controller/admController.js
+++ src/controller/admController.js
@@ -6,5 +6,15 @@ class AdmController extends Controller{
     constructor(){
         super(admServices);
     }
+    async login(req,res){
+        const {login,senha}=req.body;
+        try{
+            const adm=await admServices.login(login,senha);
+            if(adm) return res.status(200).json(adm);
+            return res.status(404).json({error:'Usuário não encontrado'});
+        }catch(error){
+            return res.status(500).json({error:error.message});
+        }    
+    } 
 }
 module.exports=AdmController;
--- src/controller/reservaController.js
+++ src/controller/reservaController.js
@@ -1,52 +1,37 @@
-const Controller = require('./controller.js');
-const dataSource = require('../models');
-const axios = require('axios');
-const Reserva = require('../models/reserva.js');
-const ReservaServices = require('../services/reservaServices.js');
-const reservaServices = new ReservaServices();
+const Controller=require('./controller.js');
+const ReservaServices=require('../services/reservaServices.js');
+const reservaServices=new ReservaServices();
 
 class ReservaController extends Controller {
-    constructor() {
+    constructor(){
         super(reservaServices);
     }
+    
     async reservasStatus(req, res) {
-        const situacao = req.params.status;
-        try {
-            const reservas = await dataSource.Reserva.findAll({where: {situacao: situacao}});
+        const situacao=req.params.status;
+        try{
+            const reservas=await reservaServices.reservaStatus(situacao);
             return res.status(200).json(reservas);
-        } catch (error) {
-            return res.status(500).json({ error: error.message });
+        }catch(error){
+            return res.status(500).json({error:error.message});
         }
     }
-
+    
     async cria(req, res) {
-        const { id_sala, dataReservada } = req.body;
-        const novoRegistro = req.body;
-        try {
-            const response = await axios.get(`http://localhost:3000/reserva/${id_sala}/${dataReservada}`);
-            if (response.status === 200) {
-                return res.status(404).json({ error: 'Sala não encontrada ou horário já reservado' });
-            }
-            const novoRegistroCriado = await this.entidadeService.criaRegistro(novoRegistro);
+        const novoRegistro=req.body;
+        try{
+            const novoRegistroCriado=await reservaServices.criaRegistro(novoRegistro);
             return res.status(201).json(novoRegistroCriado);
-        } catch (error) {
-            console.error('Erro ao criar reserva:', error);
-            return res.status(500).json({ error: 'Erro interno do servidor' });
+        }catch (error){
+            return res.status(500).json({ error: error.message});
         }
     }
 
     async verificaHorarioReservaExistente(req, res) {
-        const idSala = Number(req.params.idSala);
-        const dataReservada = (req.params.dataReservada);       
+        const {idSala, dataReservada}=req.params; 
         try {
-            const response = await axios.get(`http://localhost:3000/sala/${idSala}`);
-            const sala = response.data; 
-            if(!sala) return res.status(404).json({ error: 'Sala não encontrada' });
-            const reservas = await dataSource.Reserva.findAll({where: {id_sala: idSala}});
-            if(!reservas) return res.status(404).json({ error: 'Reserva não encontrada' });
-            const reserva = reservas.find(reserva => reserva.dataReservada === dataReservada && reserva.situacao === 'confirmada' || reserva.situacao === 'pendente');
-            if (reserva) return res.status(200).json(true);
-            return res.status(200).json(false);
+            const response=await reservaServices.verificaHorarioReservaExistente(idSala, dataReservada);
+            return res.status(200).json(response);
         } catch (error) {
             return res.status(500).json({ error: error.message });
         }
