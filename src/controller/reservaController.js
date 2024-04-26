bf085e1585a174a2fde9229ca9ef812ddf7ef9ec
Gustalex <alexandregustavo00@gmail.com>
2024-04-26 15:03:06 -0300

    remocao de comando no controller e add no services

--- src/controller/reservaController.js
+++ src/controller/reservaController.js
@@ -1,8 +1,7 @@
-const Controller=require('./controller.js');
-const ReservaServices=require('../services/reservaServices.js');
-const reservaServices=new ReservaServices();
+const Controller = require('./controller.js');
+const ReservaServices = require('../services/reservaServices.js');
+const reservaServices = new ReservaServices();
 const nodemailer = require('nodemailer');
-const { enviaWhatsApp } = require('../services/whatsappServices');
 
 class ReservaController extends Controller {
     constructor(){
@@ -23,19 +22,6 @@ class ReservaController extends Controller {
         const novoRegistro = req.body;
         try{
             const novoRegistroCriado = await reservaServices.criaRegistro(novoRegistro);
-if(novoRegistroCriado){
-    const regrasCoworking = `
-
-As chaves das salas de estudo só podem ser entregues se for realizada uma reserva prévia, que é feita por aqui mesmo, e se tiverem no mínimo 3 pessoas presentes, e no máximo 5. O tempo máximo de permanência são 3 horas. Temos um tempo de tolerância para a retirada da chave de 30 MINUTOS, passando disso, se chegar algum grupo com no mínimo 3 pessoas presentes, e pedir a sala, entregaremos.
-
-Lembrando que se ao chegar tiverem as 3 pessoas, mas durante o uso da sala uma for embora, não poderá permanecer na sala com duas pessoas e terá que devolver a chave!
-
-Dentro das salas não é permitida a alimentação, assim como em todo o espaço de coworking. Por favor, ao utilizar o coworking mantenha o máximo de silêncio possível, pois é uma área onde todos estão tentando se concentrar em suas atividades ou trabalho. Agradeço desde já!
-`;
-    let numero_cel = '+5582993540802'
-    let mensagem_txt = `Olá! Uma nova reserva foi feita. Confira as regras do coworking: ${regrasCoworking}`;
-    await enviaWhatsApp(numero_cel, mensagem_txt);
-}
             return res.status(201).json(novoRegistroCriado);
         }catch(error){
             return res.status(500).json({ error: error.message });
--- src/services/reservaServices.js
+++ src/services/reservaServices.js
@@ -1,5 +1,7 @@
 const Services=require('./services.js');
 const dataSource=require('../models');
+const { enviaWhatsApp } = require('../services/whatsappServices');
+
 
 class ReservaServices extends Services{
     constructor(){
@@ -29,7 +31,20 @@ class ReservaServices extends Services{
         try{
             const response=await this.verificaDisponibilidade(novoRegistro.id_sala, novoRegistro.dataReservada);
             if (response) return{error: 'Sala já reservada'};
-            return dataSource.Reserva.create(novoRegistro);
+            const novoRegistroCriado =  dataSource.Reserva.create(novoRegistro);
+            /*if(novoRegistroCriado){
+                const regrasCoworking = `
+
+    As chaves das salas de estudo só podem ser entregues se for realizada uma reserva prévia, que é feita por aqui mesmo, e se tiverem no mínimo 3 pessoas presentes, e no máximo 5. O tempo máximo de permanência são 3 horas. Temos um tempo de tolerância para a retirada da chave de 30 MINUTOS, passando disso, se chegar algum grupo com no mínimo 3 pessoas presentes, e pedir a sala, entregaremos.
+                
+    Lembrando que se ao chegar tiverem as 3 pessoas, mas durante o uso da sala uma for embora, não poderá permanecer na sala com duas pessoas e terá que devolver a chave!
+                
+    Dentro das salas não é permitida a alimentação, assim como em todo o espaço de coworking. Por favor, ao utilizar o coworking mantenha o máximo de silêncio possível, pois é uma área onde todos estão tentando se concentrar em suas atividades ou trabalho. Agradeço desde já!
+`;
+                let numero_cel = '+5582993640802'
+                const mensagem_txt = `Olá! Uma nova reserva foi feita. Confira as regras do coworking: ${regrasCoworking}`;
+                await enviaWhatsApp(numero_cel, mensagem_txt);
+            }*/
         }catch (error){
             throw error;
         }
