50ee666306dbd80d5988ca44f6c174f80d59c7ff
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2024-05-01 05:57:51 -0300

    Validação com zod

--- src/services/admServices.js
+++ src/services/admServices.js
@@ -1,6 +1,6 @@
-const Services = require('./services.js');
+const Services=require('./services.js');
 const dataSource=require('../models');
-
+const z=require('zod');
 class admServices extends Services {
     constructor() {
         super('Adm'); //nome do modelo
@@ -8,5 +8,16 @@ class admServices extends Services {
     async login(login, senha) {
         return dataSource.Adm.findOne({ where: {login, senha } });
     }
+    validacaoCadastro=z.object({
+        senha: z.string().min(6).max(20),
+        telefone: z.string().min(10).max(16),
+        cpf: z.string().length(11),
+    });
+
+    async criaRegistro(novoRegistro) {
+        const response=await this.validacaoCadastro.safeParseAsync(novoRegistro);
+        if (!response.success) return { error: response.error };
+        return await dataSource.Adm.create(novoRegistro);
+    }
 }
-module.exports = admServices;
+module.exports=admServices;
