13e6744f27b1dd8e025c290658953ed265374508
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2024-05-17 11:35:03 -0300

    Adição de testes para a rota de tipoUsuario

--- __tests__/tipoUsuarioTest.spec.js
+++ __tests__/tipoUsuarioTest.spec.js
@@ -2,6 +2,14 @@ const request=require('supertest');
 const app='http://localhost:3000';
 
 describe('Teste das rotas de tipoUsuario', ()=>{
+    const atributos = ['tipoUser', 'glossarioTipo'];
+    const tipoUsuario={
+        id:5,
+        tipoUser: 5,
+        glossarioTipo: "Enéas um deus",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    };
     it('Deve listar todos os tipoUsuarios', async () => {
         const response = await request(app).get('/tipoUsuario');
         expect(response.status).toBe(200);
@@ -13,17 +21,27 @@ describe('Teste das rotas de tipoUsuario', ()=>{
     it('Deve criar uma nova tipoUsuario', async () => {
         const response = await request(app)
             .post('/tipoUsuario')
-            .send({});
+            .send(tipoUsuario);
         expect(response.status).toBe(201); 
     });
     it('Deve atualizar uma tipoUsuario existente', async () => {
         const response = await request(app)
             .put('/tipoUsuario/1')
-            .send({});
+            .send({glossarioTipo:"Enéas é foda", updatedAt: new Date()});
         expect(response.status).toBe(200);
     });
+    atributos.forEach(atributo => {
+        it(`Deve retornar erro 400 ao tentar criar uma tipoUsuario sem o atributo '${atributo}'`, async () => {
+            const novaTipoUsuario = { ...tipoUsuario };
+            delete novaTipoUsuario[atributo];
+            const response = await request(app)
+                .put('/tipoUsuario/1')
+                .send(novaTipoUsuario);
+            expect(response.status).toBe(500);
+        });
+    });
     it('Deve deletar uma tipoUsuario existente', async () => {
-        const response = await request(app).delete('/tipoUsuario/1'); 
+        const response = await request(app).delete('/tipoUsuario/5'); 
         expect(response.status).toBe(200); 
     });
 });
