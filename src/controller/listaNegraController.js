7d9cd3f3dcd4c816063a3525567bd584588edec1
Gustalex <alexandregustavo00@gmail.com>
2024-05-17 14:35:07 -0300

    feat: add logica de lista negra

--- src/controller/listaNegraController.js
+++ src/controller/listaNegraController.js
@@ -6,5 +6,18 @@ class listaNegraController extends Controller{
     constructor(){
         super(listaNegraServices);
     }
+
+    async validarResponsavel(req, res){
+        const idResponsavel = req.params.idResponsavel;
+        try{
+            const responsavelNaListaNegra = await listaNegraServices.consultarResponsavel(idResponsavel);
+            if(responsavelNaListaNegra){
+                return res.status(403).json({error: 'O usuário está bloqueado de fazer reservas!'})
+            }
+            return res.status(200).json({valido: true});
+        }catch(error){
+            return res.status(500).json({error:error.message});
+        }
+    }
 }
 module.exports=listaNegraController;
--- src/services/listaNegraServices.js
+++ src/services/listaNegraServices.js
@@ -1,8 +1,13 @@
 const Services = require('./services.js');
+const dataSource = require('../models');
 
 class listaNegraServices extends Services {
     constructor() {
         super('ListaNegra'); //nome do modelo
+    }
+
+    async consultarResponsavel(idResponsavel){
+        return await dataSource.ListaNegra.findOne({where:{idResponsavel:idResponsavel}});
     }
 }
 module.exports = listaNegraServices;
