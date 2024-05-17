695684937509474969b76ed5a2bf02e55ac2194a
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2024-05-17 11:24:22 -0300

    Adição de testes aprimorados de rotas de reuniã

--- __tests__/reuniaoTest.spec.js
+++ __tests__/reuniaoTest.spec.js
@@ -2,6 +2,14 @@ const request=require('supertest');
 const app='http://localhost:3000';
 
 describe('Teste das rotas de reuniao', ()=>{
+    const atributos = ['reservaId', 'idParticipante'];
+    const reuniao = {
+        id: 2,
+        reservaId: 1,
+        idParticipante: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    };
     it('Deve listar todas as reuniaos', async () => {
         const response = await request(app).get('/reuniao');
         expect(response.status).toBe(200);
@@ -13,17 +21,27 @@ describe('Teste das rotas de reuniao', ()=>{
     it('Deve criar uma nova reuniao', async () => {
         const response = await request(app)
             .post('/reuniao')
-            .send({});
+            .send(reuniao);
         expect(response.status).toBe(201); 
     });
     it('Deve atualizar uma reuniao existente', async () => {
         const response = await request(app)
-            .put('/reuniao/1')
-            .send({});
+            .put('/reuniao/2')
+            .send({idParticipante:2,updatedAt: new Date()});
         expect(response.status).toBe(200);
     });
+    atributos.forEach((atributo) => {
+        it(`Deve retornar erro 400 ao tentar criar uma reuniao sem o atributo '${atributo}'`, async () => {
+            const novaReuniao = { ...reuniao };
+            delete novaReuniao[atributo];
+            const response = await request(app)
+                .post('/reuniao')
+                .send(novaReuniao);
+            expect(response.status).toBe(500);
+        });
+    });
     it('Deve deletar uma reuniao existente', async () => {
-        const response = await request(app).delete('/reuniao/1'); 
+        const response = await request(app).delete('/reuniao/2'); 
         expect(response.status).toBe(200); 
     });
 });
