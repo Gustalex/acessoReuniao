18ee9a7970d5bc2a1352a10538299c7fa43f198d
Gustalex <alexandregustavo00@gmail.com>
2024-05-21 12:27:48 -0300

    estrutura o reserva controller para o disparo do email mediante a confirmacao da acao

--- src/controller/reservaController.js
+++ src/controller/reservaController.js
@@ -1,7 +1,8 @@
 const Controller=require('./controller.js');
 const ReservaServices=require('../services/reservaServices.js');
 const reservaServices=new ReservaServices();
-const { enviaWhatsApp } = require('../services/whatsappServices');
+const { sendMail } = require('../services/emailServices.js');
+const emailBody = require('../utils/emailTemplate.js');
 
 class ReservaController extends Controller {
     constructor(){
@@ -42,19 +43,18 @@ class ReservaController extends Controller {
         const novoRegistro = req.body;
         try{
             const novoRegistroCriado = await reservaServices.criaRegistro(novoRegistro);
-            /*if(novoRegistroCriado){
-                const regrasCoworking = `
-
-            As chaves das salas de estudo só podem ser entregues se for realizada uma reserva prévia, que é feita por aqui mesmo, e se tiverem no mínimo 3 pessoas presentes, e no máximo 5. O tempo máximo de permanência são 3 horas. Temos um tempo de tolerância para a retirada da chave de 30 MINUTOS, passando disso, se chegar algum grupo com no mínimo 3 pessoas presentes, e pedir a sala, entregaremos.
-
-            Lembrando que se ao chegar tiverem as 3 pessoas, mas durante o uso da sala uma for embora, não poderá permanecer na sala com duas pessoas e terá que devolver a chave!
-
-            Dentro das salas não é permitida a alimentação, assim como em todo o espaço de coworking. Por favor, ao utilizar o coworking mantenha o máximo de silêncio possível, pois é uma área onde todos estão tentando se concentrar em suas atividades ou trabalho. Agradeço desde já!
-            `;
-                let numero_cel = '+5582993540802'
-                let mensagem_txt = `Olá! Uma nova reserva foi feita. Confira as regras do coworking: ${regrasCoworking}`;
-                await enviaWhatsApp(numero_cel, mensagem_txt);
-            }*/
+            const reservista = await reservaServices.buscarReservista(novoRegistro.id_reservista);
+            if(novoRegistroCriado){
+                try{
+                    const subject = 'Confirmação de Reserva CIPT'
+                    const message = emailBody;
+                    const email = reservista.email
+                    await sendMail(subject, message, email);
+                }catch(erro){
+                    console.error(`Erro ao enviar o email: ${erro}`);
+                    throw erro;
+                }
+            }
             return res.status(200).json(novoRegistroCriado);
         }catch(erro){
             return res.status(500).json({message: erro.message,name: erro.name,stack:erro.stack});
