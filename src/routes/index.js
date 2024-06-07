3bcc03737c0964ca803c06d658df2292633d884f
Enéas <david00eneas@gmail.com>
2024-06-06 23:11:02 -0300

    correção de routes no index da pasta route, para permitir acesso aos funções

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,27 @@
 const express = require('express');
 
+const estadoSalaRoutes = require('./estadoSalaRoutes.js');
 const fracasoRoutes=require('./fracasoRoutes.js');
 const listaNegraRoutes=require('./listaNegraRoutes.js');
 const nivelAcessoRoutes = require('./nivelAcessoRoutes.js');
-const estadoSalaRoutes = require('./estadoSalaRoutes.js');
+const recepcionistaRoutes = require('./recepcionistaRoutes.js');
 const reservaRoutes = require('./reservaRoutes.js');
 const reuniaoRoutes = require('./reuniaoRoutes.js');
 const salaRoutes = require('./salaRoutes.js');
 const usuarioRoutes = require('./usuarioRoutes.js');
-const recepcionistaRoutes = require('./recepcionistaRoutes.js');
 
 
 const routes = (app) => {
     app.use(express.json());
-    app.use(nivelAcessoRoutes);
-    app.use(reuniaoRoutes);
     app.use(estadoSalaRoutes);
-    app.use(salaRoutes);
-    app.use(reservaRoutes);
-    app.use(usuarioRoutes);
-    app.use(participanteRoutes);
     app.use(fracasoRoutes);
-    app.use(listaNegraRoutes);
+    app.use(listaNegraRoutes);    
+    app.use(nivelAcessoRoutes);
     app.use(recepcionistaRoutes);
-
+    app.use(reuniaoRoutes);
+    app.use(reservaRoutes);
+    app.use(salaRoutes);
+    app.use(usuarioRoutes);
 };
 
 module.exports = routes;
