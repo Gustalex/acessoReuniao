9f9de149d7c292fdca9731b76f5d19c708a50e5d
Enéas <david00eneas@gmail.com>
2024-06-06 22:31:42 -0300

    Services de nivelAcesso com enfoco na mensagem de erro

--- src/services/nivelAcessoServices.js
+++ src/services/nivelAcessoServices.js
@@ -5,9 +5,8 @@ const z=require('zod');
 class NivelAcessoServices extends Services{
     constructor(){
         super('NivelAcesso',z.object({
-            descricao:z.string().min(5,{message:"O campo de descrição necessita de NO MINIMO 5 caracteres"}).max(255,{message:"O campo de descrição necessita de NO MAXIMO 255 caracteres"}),
-            createdAt:z.string().date({message:"O campo de data de criação necessita do time yyyy-mm-dd"}),
-            updatedAt:z.string().date({message:"O campo de data de atualização necessita do time yyyy-mm-dd"}),
+            nivelAcesso:z.string().min(5,{message:"O campo de nivel de acesso necessita de NO MINIMO 5 caracteres"}).max(255,{message:"O campo de nivel de acesso de NO MAXIMO 255 caracteres"}),
+            glossarioNivel:z.string().min(5,{message:"O campo de descrição de nivel de acesso necessita de NO MINIMO 5 caracteres"}).max(255,{message:"O campo de descrição de nivel de acesso  necessita de NO MAXIMO 255 caracteres"}),
         }));
     }
 }
