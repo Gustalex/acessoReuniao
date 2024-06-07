0e7a833ec84b265843bfb9a0e34de55042372359
Enéas <david00eneas@gmail.com>
2024-06-06 23:08:36 -0300

    correção de routes no index da pasta route, para permitir acesso aos funções

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,25 +3,26 @@ const express = require('express');
 const fracasoRoutes=require('./fracasoRoutes.js');
 const listaNegraRoutes=require('./listaNegraRoutes.js');
 const nivelAcessoRoutes = require('./nivelAcessoRoutes.js');
-const participanteRoutes = require('./participanteRoutes.js');
+const estadoSalaRoutes = require('./estadoSalaRoutes.js');
 const reservaRoutes = require('./reservaRoutes.js');
 const reuniaoRoutes = require('./reuniaoRoutes.js');
 const salaRoutes = require('./salaRoutes.js');
-const tipoUsuarioRoutes = require('./tipoUsuarioRoutes.js');
-const userRoutes = require('./userRoutes.js');
+const usuarioRoutes = require('./usuarioRoutes.js');
+const recepcionistaRoutes = require('./recepcionistaRoutes.js');
 
 
 const routes = (app) => {
     app.use(express.json());
     app.use(nivelAcessoRoutes);
     app.use(reuniaoRoutes);
-    app.use(tipoUsuarioRoutes);
+    app.use(estadoSalaRoutes);
     app.use(salaRoutes);
     app.use(reservaRoutes);
-    app.use(userRoutes);
+    app.use(usuarioRoutes);
     app.use(participanteRoutes);
     app.use(fracasoRoutes);
     app.use(listaNegraRoutes);
+    app.use(recepcionistaRoutes);
 
 };
 
