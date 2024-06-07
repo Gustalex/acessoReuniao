fe780b546bb0a31ab5be45f165065a246cf0ed98
Enéas <david00eneas@gmail.com>
2024-06-06 22:47:06 -0300

    Adição de services com enfoco em mensagem de erros de reuniao

---
+++ src/services/reuniaoServices.js
@@ -0,0 +1,12 @@
+const Services=require('./services,js');
+const z=require('zod');	
+
+class ReuniaoServices extends Services{
+    constructor(){
+        super('Reuniao',z.object({
+            reservaId:z.number().int({message:"O campo de reservaId necessita ser um numero inteiro"}).positive({message:"O campo de reservaId necessita ser um numero inteiro positivo"}),
+            idParticipante:z.array(z.number().int({message:"O campo de idParticipante necessita ser um numero inteiro"}).positive({message:"O campo de idParticipante necessita ser um numero inteiro positivo"})),
+        }));
+    }
+}
+module.exports=ReuniaoServices;
